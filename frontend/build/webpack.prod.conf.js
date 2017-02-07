f282444f0e3731b7c9fea503ee9a85180285174e
Ivan Feofanov <feofanov@gmail.com>
2017-02-07 13:40:19 +0500

    fix prerender

--- frontend/build/webpack.prod.conf.js
+++ frontend/build/webpack.prod.conf.js
@@ -6,6 +6,7 @@ var merge = require('webpack-merge')
 var baseWebpackConfig = require('./webpack.base.conf')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var PrerenderSpaPlugin = require('prerender-spa-plugin')
 var env = config.build.env
 
 var webpackConfig = merge(baseWebpackConfig, {
@@ -69,7 +70,13 @@ var webpackConfig = merge(baseWebpackConfig, {
     new webpack.optimize.CommonsChunkPlugin({
       name: 'manifest',
       chunks: ['vendor']
-    })
+    }),
+    new PrerenderSpaPlugin(
+      // Absolute path to compiled SPA
+      config.build.assetsRoot,
+      // List of routes to prerender [ '/', '/about', '/contact' ]
+      [ '/' ]
+    )
   ]
 })
 
