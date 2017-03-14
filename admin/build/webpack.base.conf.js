69a617014a3d3a3b2aa8e825536fb9220a0bdd44
Ivan Feofanov <feofanov@gmail.com>
2017-03-14 12:09:41 +0500

    AP switch to vue, frontend Vue upgrade to 2.2.4

--- admin/build/webpack.base.conf.js
+++ admin/build/webpack.base.conf.js
@@ -23,7 +23,7 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
-      'components': resolve('src/components'),
+      'components': resolve('src/components')
     }
   },
   module: {
---
+++ frontend/src/components/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="index">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index'
+}
+</script>
+
+<style>
+</style>
