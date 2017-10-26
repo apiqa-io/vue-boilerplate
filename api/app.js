f08a4de195685602486f04bd28621f9cfb5a1543
Ivan Feofanov <feofanov@gmail.com>
2017-10-26 12:54:48 +0500

    add logging to api service

--- api/app.js
+++ api/app.js
@@ -1,5 +1,9 @@
+const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const rfs = require('rotating-file-stream')
 
 const passport = require('./passport')
 const adminRoutes = require('./routes/admin')
@@ -7,8 +11,21 @@ const mainRoutes = require('./routes/main')
 
 const app = express()
 
-app.disable('x-powered-by')
+app.use(morgan('dev', {
+  skip: function (req, res) { return res.statusCode < 400 }
+}))
 
+if (process.env.ENVIRONMENT !== 'dev') {
+  const logDirectory = path.join(__dirname, 'log')
+
+  // create a rotating write stream
+  const accessLogStream = rfs('api.log', {
+    interval: '1d', // rotate daily
+    path: logDirectory
+  })
+  app.use(morgan('combined', {stream: accessLogStream}))
+}
+app.use(helmet())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -24,7 +41,7 @@ app.use((req, res, next) => {
 })
 
 app.use((err, req, res, next) => {
-  console.error(err)
+  console.error(err) // eslint-disable-line no-console
   res
     .status(err.status || 500)
     .json({
@@ -34,4 +51,4 @@ app.use((err, req, res, next) => {
 })
 
 const PORT = 80
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`)) // eslint-disable-line no-console
